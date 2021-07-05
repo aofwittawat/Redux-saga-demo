5c604fbb27baebf934c16676d0602ff6c2b40acd
Wittawat Boonyanuwat <aofwittawat@gmail.com>
2021-07-05 20:07:19 +0700

    redux-saga-demo finish

---
+++ src/redux/saga/index.js
@@ -0,0 +1,31 @@
+
+import { takeEvery, all } from 'redux-saga/effects'
+import { INCREMENT_REQ, DECREMENT_REQ, ADD_REQ, INCREMENT_ASYNC_REQ } from '../actionTypes'
+import { setIncrementAction, setDecrementAction, setAddAction, setIncrementAsyncAction } from './counter.action'
+
+
+function* watchIncrementAction(){
+    yield takeEvery(INCREMENT_REQ, setIncrementAction)
+}
+
+function* watchIncrementAsyncAction(){
+    yield takeEvery(INCREMENT_ASYNC_REQ, setIncrementAsyncAction)
+}
+
+ function* watchDecrementAction(){
+    yield takeEvery(DECREMENT_REQ, setDecrementAction)
+}
+
+ function* watchAddAction(){
+    yield takeEvery(ADD_REQ, setAddAction)
+}
+
+// รวมแต่ละ watch ก่อน export ไปที่ index.js
+export default function* rootSaga(){
+    yield all([
+        watchIncrementAction(),
+        watchIncrementAsyncAction(),
+        watchDecrementAction(),
+        watchAddAction()]
+    )
+}
